6f12f57034c8e9748ec2be43186c6d0b4ea8fb40
vladyslavgorban <vladislav.gorban@gmail.com>
2026-02-22 13:06:14 +0100

    workin code for the first metrics before tibulen help

---
+++ basic trx query.sql
@@ -0,0 +1,52 @@
+WITH trx_details AS (SELECT
+                         pi.created_at                                                       transaction_dttm
+                       , pi.player_id
+                       , pi.id                                                               invoice_id
+                       , CASE
+                             WHEN pi.type IN (1, 4) AND pi.number = 1 AND pi.status = 2
+                                 THEN pi.created_at
+                             END                                                          AS fd_dttm
+                       , CASE WHEN pi.type IN (1, 4) AND pi.status = 2 THEN pi.number END AS deposit_number
+--                        , pp.is_test                                                          test_account
+--                        , pp.country                                                          user_county
+--                        , pp.affiliate_tag
+                       , ppm.method_name                                                  AS pay_method
+                       , CASE
+                             WHEN pi.type IN (1, 4) THEN 'deposit'
+                             WHEN pi.type IN (2, 5) THEN 'withdrowal'
+                             ELSE pi.type :: VARCHAR
+                             END                                                          AS transaction_type
+                       , CASE
+                             WHEN pi.status = 2 THEN 'success'
+                             WHEN pi.status != 1 AND
+                                  COALESCE(pi.status_result, '') NOT IN
+                                  ('Cancelled by Timeout', 'Cancelled by Customer')
+                                 THEN 'pending'
+                             WHEN pi.status = 1 THEN 'failed'
+                             ELSE
+                                 'status: ' || pi.status :: VARCHAR ||
+                                 ' (' || COALESCE(pi.status_result, 'no details') || ')'
+                             END                                                          AS transaction_status
+                       , ppd.payment_provider
+                       , ppd.payment_method                                                  prvider_pay_method
+                     FROM stealthcasino_payment_service.public.invoices pi
+                              --                               LEFT JOIN stealthcasino_player_service.public.players pp
+--                                         ON pp.id = pi.player_id
+                              LEFT JOIN stealthcasino_payment_service.public.payment_methods ppm
+                                        ON ppm.id = pi.payment_method_id
+                              LEFT JOIN stealthcasino_payment_service.public.payment_details ppd
+                                        ON ppd.id = pi.payment_details_id
+-- LIMIT 300;
+)
+SELECT
+    COUNT(*)
+FROM trx_details
+;
+SELECT
+    pi.type
+  , *
+FROM stealthcasino_payment_service.public.invoices pi
+WHERE 1 = 1
+  AND pi.number = 1
+LIMIT 300
+
