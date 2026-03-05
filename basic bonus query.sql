098577deec0aecf9610ff452f0388f7908e942ed
vladyslavgorban <vladislav.gorban@gmail.com>
2026-03-05 16:31:59 +0100

    add basic bonus query

---
+++ basic bonus query.sql
@@ -0,0 +1,17 @@
+-- basic bonus query
+
+SELECT 
+pb.created_at as dttm
+,pb.name as bonus_name
+,pb.bonus_id
+,pb.player_id
+,pb.id as operation_id
+,case pb.result
+    when 1 then 'pending'
+    when 2 then 'activated'
+    when 3 then 'cancelled'
+    when 4 then 'expired'
+    when 5 then 'lost'
+end as bonus_result
+,ROUND(1.0 * pb.base_currency_amount / 100, 2) as bonus_sum
+FROM public.player_bonuses pb
