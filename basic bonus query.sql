b1d02c198e2b8811f7bbf35f86b2de0f2bc52634
vladyslavgorban <vladislav.gorban@gmail.com>
2026-03-16 09:38:00 +0100

    added win and cancel sum, as well as currency to basic bonus

--- basic bonus query.sql
+++ basic bonus query.sql
@@ -1,17 +1,20 @@
 -- basic bonus query
 
-SELECT 
-pb.created_at as dttm
-,pb.name as bonus_name
-,pb.bonus_id
-,pb.player_id
-,pb.id as operation_id
-,case pb.result
-    when 1 then 'pending'
-    when 2 then 'activated'
-    when 3 then 'cancelled'
-    when 4 then 'expired'
-    when 5 then 'lost'
-end as bonus_result
-,ROUND(1.0 * pb.base_currency_amount / 100, 2) as bonus_sum
+SELECT pb.created_at                                          as dttm
+     , pb.name                                                as bonus_name
+     , pb.currecy
+     , pb.bonus_id
+     , pb.player_id
+     , pb.id                                                  as operation_id
+     , pb.tag
+     , case pb.result
+           when 1 then 'pending'
+           when 2 then 'activated'
+           when 3 then 'cancelled'
+           when 4 then 'expired'
+           when 5 then 'lost'
+    end                                                       as bonus_result
+     , ROUND(1.0 * pb.base_currency_amount / 100, 2)          as bonus_sum
+     , ROUND(1.0 * pb.base_currency_payout / 100, 2)          as bonus_win_sum
+     , round(1.0 * pb.base_currency_canceled_payout / 100, 2) as bonus_cancel_sum
 FROM public.player_bonuses pb
